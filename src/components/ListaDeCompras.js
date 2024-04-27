023f2dd1fa75916ec8a6b7571ccbba52d490e4ea
Rodrigo Santos <rodrigo7x@gmail.com>
2024-04-27 16:56:03 -0300

    alterado para apresentar o nome Valor no campo de valor e nao o numero 0

--- src/components/ListaDeCompras.js
+++ src/components/ListaDeCompras.js
@@ -6,7 +6,7 @@ import './ListaDeCompras.css';
 const ListaDeCompras = () => {
   const [items, setItems] = useState([]);
   const [itemName, setItemName] = useState('');
-  const [itemValue, setItemValue] = useState(0);
+  const [itemValue, setItemValue] = useState('');
   const [editItemId, setEditItemId] = useState(null);
 
   const handleAddItem = () => {
