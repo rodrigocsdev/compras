609fd13f4e37d780a9335853d0c594da7d3eae8a
rodrigocsdev <rodrigo7x@gmail.com>
2024-07-05 09:09:12 -0300

    add filtro de nome e  ordenar items tem tela

--- src/components/ListaDeCompras.js
+++ src/components/ListaDeCompras.js
@@ -1,99 +1,95 @@
-// src/components/ListaDeCompras.js
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ListaDeCompras.css";
 
 const ListaDeCompras = () => {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("listaDeCompras")) || []
-  );
+  const initialItems = JSON.parse(localStorage.getItem("listaDeCompras")) || [];
+  const [items, setItems] = useState(initialItems);
   const [itemName, setItemName] = useState("");
   const [itemValue, setItemValue] = useState("");
   const [itemQuantity, setItemQuantity] = useState("");
   const [editItemId, setEditItemId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortField, setSortField] = useState(null);
+  const [sortDirection, setSortDirection] = useState("asc");
+
+  useEffect(() => {
+    localStorage.setItem("listaDeCompras", JSON.stringify(items));
+  }, [items]);
 
   const handleAddItem = () => {
     if (itemName && itemValue && itemQuantity) {
+      const newItem = {
+        id: items.length > 0 ? items[items.length - 1].id + 1 : 1,
+        name: itemName,
+        value: parseFloat(itemValue),
+        quantity: parseInt(itemQuantity),
+      };
+
       if (editItemId !== null) {
-        // Se estamos editando um item, atualizamos o item existente na lista
-        const updatedItems = items.map((item) => {
-          if (item.id === editItemId) {
-            return {
-              ...item,
-              name: itemName,
-              value: parseFloat(itemValue),
-              quantity: parseInt(itemQuantity),
-            };
-          }
-          return item;
-        });
+        const updatedItems = items.map((item) =>
+          item.id === editItemId ? { ...newItem, id: editItemId } : item
+        );
         setItems(updatedItems);
-        setEditItemId(null); // Resetar o ID de edição
-
-        // Salvar a lista de compras no localStorage
-        localStorage.setItem("listaDeCompras", JSON.stringify(updatedItems));
+        setEditItemId(null);
       } else {
-        // Caso contrário, adicionamos um novo item à lista
-        setItems([
-          ...items,
-          {
-            id: Date.now(),
-            name: itemName,
-            value: parseFloat(itemValue),
-            quantity: parseInt(itemQuantity),
-          },
-        ]);
-
-        // Salvar a lista de compras no localStorage
-        const newItems = [
-          ...items,
-          {
-            id: Date.now(),
-            name: itemName,
-            value: parseFloat(itemValue),
-            quantity: parseInt(itemQuantity),
-          },
-        ];
-        localStorage.setItem("listaDeCompras", JSON.stringify(newItems));
+        setItems([...items, newItem]);
       }
-      // Limpar campos de entrada
+
       setItemName("");
       setItemValue("");
       setItemQuantity("");
-      inputRef.current.focus(); // adicione essa linha
     }
   };
 
   const handleRemoveItem = (id) => {
     setItems(items.filter((item) => item.id !== id));
-    // Salvar a lista de compras no localStorage
-    localStorage.setItem(
-      "listaDeCompras",
-      JSON.stringify(items.filter((item) => item.id !== id))
-    );
   };
 
   const handleEditItem = (id) => {
     const itemToEdit = items.find((item) => item.id === id);
     if (itemToEdit) {
-      // Preencher os campos de entrada com os valores do item a ser editado
       setItemName(itemToEdit.name);
       setItemValue(itemToEdit.value.toString());
       setItemQuantity(itemToEdit.quantity.toString());
-      // Definir o ID do item sendo editado
       setEditItemId(id);
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
     }
   };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSort = (field) => {
+    if (sortField === field) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortField(field);
+      setSortDirection("asc");
+    }
+  };
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortField) {
+      const fieldA = a[sortField];
+      const fieldB = b[sortField];
+
+      if (sortDirection === "asc") {
+        return fieldA > fieldB ? 1 : fieldA < fieldB ? -1 : 0;
+      } else {
+        return fieldA < fieldB ? 1 : fieldA > fieldB ? -1 : 0;
+      }
+    } else {
+      return 0;
+    }
+  });
+
+  const filteredItems = sortedItems.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   const calculateTotal = () => {
     return items.reduce((total, item) => total + item.value * item.quantity, 0);
   };
-
-  const inputRef = React.createRef();
 
   return (
     <div className="container">
@@ -106,8 +102,6 @@ const ListaDeCompras = () => {
           placeholder="Nome do item"
           value={itemName}
           onChange={(e) => setItemName(e.target.value)}
-          ref={inputRef}
-          autoFocus={false} // remova o autoFocus aqui
         />
         <input
           type="number"
@@ -125,36 +119,51 @@ const ListaDeCompras = () => {
           {editItemId !== null ? "Atualizar" : "Adicionar"}
         </button>
       </div>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Pesquisar por nome"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
       <table className="items-table">
-  <thead>
-    <tr>
-      <th>Nome</th>
-      <th>Valor</th>
-      <th>Qtd</th>
-      <th>Total</th>
-      <th>Ações</th>
-    </tr>
-  </thead>
-  <tbody>
-    {items.map((item) => (
-      <tr key={item.id}>
-        <td> {item.name} </td>
-        <td> R${item.value.toFixed(2)} </td>
-        <td> {item.quantity} </td>
-        <td> R${(item.value * item.quantity).toFixed(2)} </td>
-        <td>
-          <button className="edit-button" onClick={() => handleEditItem(item.id)}>
-            Editar
-          </button>
-          <button className="remove-button" onClick={() => handleRemoveItem(item.id)}>
-            Remover
-          </button>
-        </td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("id")}>
+              ID {sortField === "id" && (sortDirection === "asc" ? "▲" : "▼")}
+            </th>
+            <th onClick={() => handleSort("name")}>
+              Nome {sortField === "name" && (sortDirection === "asc" ? "▲" : "▼")}
+            </th>
+            <th onClick={() => handleSort("value")}>
+              Valor {sortField === "value" && (sortDirection === "asc" ? "▲" : "▼")}
+            </th>
+            <th>Qtd</th>
+            <th>Total</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>R${item.value.toFixed(2)}</td>
+              <td>{item.quantity}</td>
+              <td>R${(item.value * item.quantity).toFixed(2)}</td>
+              <td>
+                <button className="edit-button" onClick={() => handleEditItem(item.id)}>
+                  Editar
+                </button>
+                <button className="remove-button" onClick={() => handleRemoveItem(item.id)}>
+                  Remover
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <div className="total">Total: R${calculateTotal().toFixed(2)}</div>
     </div>
   );
